f978878587e754d6c8f8d989933406a29af85343
ImranOpenFin <37070624+ImranOpenFin@users.noreply.github.com>
2020-10-20 16:32:30 +0100

    Revert "DotNet Application with Zoom"
    This reverts commit 75f46296ec4d66d995e51400998c1d67d3793cdc.

---
+++ OpenFinDotNetFramework/OpenFinDotNetFramework/Program.cs
@@ -0,0 +1,43 @@
+﻿using Openfin.Desktop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace OpenFinDotNetFramework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RuntimeOptions opts = new RuntimeOptions()
+            {
+                Version = "16.83.52.32",
+                UUID = "OpenFinDotNetFramework"
+            };
+
+            Runtime _instance = Runtime.GetRuntimeInstance(opts);
+            _instance.Connect(() =>
+            {
+                Console.WriteLine("openfin connected!");
+                Application app = _instance.CreateApplication(new ApplicationOptions("OpenFinDotNetFrameworkApp", "OpenFinDotNetFrameworkApp", "http://localhost:8080/index.html")
+                {
+                    MainWindowOptions = new WindowOptions("OpenFinDotNetFrameworkApp")
+                    {
+                        URL = "http://localhost:8080/index.html",
+                        AutoShow = true,
+                        PreloadScripts = new List<PreloadScript> { new PreloadScript("http://localhost:8080/preload/preload.js", true) }
+                    }
+                });
+                app.Start();
+            });
+
+            AutoResetEvent autoEvent = new AutoResetEvent(false);
+            autoEvent.WaitOne();
+            
+        }
+    }
+}
